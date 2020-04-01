0bb6cab771479060893b69ec1229a3edca1243d8
fadi Findakly <fadi.findakly01@stclairconnect.ca>
2020-04-01 12:46:47 -0400

    Equipment class created

--- app/src/main/java/ca/mealbell/javabeans/Equipement.java
+++ app/src/main/java/ca/mealbell/javabeans/Equipement.java
@@ -1,4 +1,51 @@
 package ca.mealbell.javabeans;
 
-public class Equipement {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Equipement implements Parcelable {
+
+    private String name;
+    private String image;
+
+    public Equipement(String name, String image) {
+        this.name = name;
+        this.image = image;
+    }
+
+    protected Equipement(Parcel in) {
+        name = in.readString();
+        image = in.readString();
+    }
+
+    public static final Creator<Equipement> CREATOR = new Creator<Equipement>() {
+        @Override
+        public Equipement createFromParcel(Parcel in) {
+            return new Equipement(in);
+        }
+
+        @Override
+        public Equipement[] newArray(int size) {
+            return new Equipement[size];
+        }
+    };
+
+    public String getName() {
+        return name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(name);
+        dest.writeString(image);
+    }
 }
