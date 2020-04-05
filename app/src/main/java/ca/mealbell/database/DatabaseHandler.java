92b04f9464ef90505c5aaf7758cfa037fec901f6
Ali Dali <ali.dali01@stclairconnect.ca>
2020-04-05 01:35:28 -0400

    Created Recipe Table * Get All Recipes * Add Recipe * Delete Recipe * Get Recipe

--- app/src/main/java/ca/mealbell/database/DatabaseHandler.java
+++ app/src/main/java/ca/mealbell/database/DatabaseHandler.java
@@ -1,10 +1,17 @@
 package ca.mealbell.database;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ca.mealbell.javabeans.RecipeInformation;
 
 public class DatabaseHandler extends SQLiteOpenHelper {
 
@@ -79,4 +86,74 @@ public class DatabaseHandler extends SQLiteOpenHelper {
         // Create tables again
         onCreate(db);
     }
+
+
+    public List<RecipeInformation> getAllRecipes() {
+        List<RecipeInformation> recipes = new ArrayList<>();
+
+        String query = "SELECT  * FROM " + TABLE_RECIPES;
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(query, null);
+
+        while (cursor.moveToNext()) {
+            int id = cursor.getInt(0);
+            String title = cursor.getString(1);
+            String image = cursor.getString(2);
+            String summary = cursor.getString(3);
+            String instructions = cursor.getString(4);
+            int readyInMinutes = cursor.getInt(5);
+            int servings = cursor.getInt(6);
+
+            //TODO: Add Ingredients and Equipments
+            RecipeInformation recipe = new RecipeInformation(id, title, image, summary, instructions, null, null, readyInMinutes, servings);
+            recipes.add(recipe);
+        }
+        cursor.close();
+        db.close();
+        return recipes;
+    }
+
+    public void addRecipe(RecipeInformation recipe) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_ID, recipe.getId());
+        values.put(COLUMN_TITLE, recipe.getTitle());
+        values.put(COLUMN_IMAGE, recipe.getImage());
+        values.put(COLUMN_SUMMARY, recipe.getSummary());
+        values.put(COLUMN_INSTRUCTIONS, recipe.getInstructions());
+        values.put(COLUMN_READY_IN_MINUTES, recipe.getReadyInMinutes());
+        values.put(COLUMN_SERVINGS, recipe.getServings());
+        //TODO: Ingredients and Equipments
+
+        db.insert(TABLE_RECIPES, null, values);
+        db.close();
+    }
+
+    public void deleteRecipe(RecipeInformation recipe) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(TABLE_RECIPES, COLUMN_ID + " = ?",
+                new String[]{String.valueOf(recipe.getId())});
+        db.close();
+    }
+
+    // TODO: Delete this useless method
+    public RecipeInformation getRecipe(int id) {
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.query(TABLE_RECIPES, new String[]{COLUMN_ID, COLUMN_TITLE, COLUMN_IMAGE, COLUMN_SUMMARY, COLUMN_INSTRUCTIONS, COLUMN_READY_IN_MINUTES, COLUMN_SERVINGS},
+                COLUMN_ID + "=?", new String[]{String.valueOf(id)}, null, null, null, null);
+        if (cursor != null)
+            cursor.moveToFirst();
+
+
+        // TODO: get recipe
+//        Contact contact = new Contact(Integer.parseInt(cursor.getString(0)),
+//                cursor.getString(1), cursor.getString(2));
+        // return contact
+        return null;
+    }
+
+
 }
